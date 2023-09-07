e6f429ec6baaf322c029909f0d5441a7e3deb31e
Krzysztof <krzys.zolnowski@gmail.com>
2023-09-07 15:02:15 +0200

    Module 3 task 5 done

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -1,6 +1,15 @@
 package lang.print.gaps.task5;
 
+import java.io.OutputStream;
+
 public class NumberDivider {
+
     void divide(int toBeDivided) {
+
+        int divider = 5;
+        double output = (double) toBeDivided / divider;
+        System.out.println(output);
     }
+
+
 }
